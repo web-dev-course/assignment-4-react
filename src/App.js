f883302f74a61211670fc53c14eb0e327ff04fcf
Muhammad Ali Haris Khan <89762447+mak-ahoy@users.noreply.github.com>
2024-08-09 16:10:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
-import Main from './components/Main';
-import Blog from './pages/Blog';
 
 function App() {
   return (
